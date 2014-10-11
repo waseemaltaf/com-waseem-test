0ca10577fcb67eef50a5fe2fbe662fcae713e5f3
waseemaltaf <waseemaltaf@yahoo.com>
2014-10-11 19:42:12 -0400

    sds

--- com-waseem/src/main/java/com/waseem/Test2.java
+++ com-waseem/src/main/java/com/waseem/Test2.java
@@ -1,11 +1,9 @@
 package com.waseem;
 
-public class Test {
+public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("my first class");
 
 	}
 
